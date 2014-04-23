32444b9cb7c0ced75bf5b021692a3058cdef4041
Aaron Davis <magwe25@gmail.com>
2014-04-22 23:12:35 -0400

    Fixing MainActivity with the actual version Nicole wrote

--- src/com/example/nannycam/MainActivity.java
+++ src/com/example/nannycam/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.nannycam;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,63 +25,107 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
+   private EditText et_input;
+   private boolean connected = false;
+   private static final int SERVER_PORT = 8080;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
 
-	private static final String TAG = "MainActivity";
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
     public void connect(View v) {
-    	// get what was typed and print it to log
-    	EditText mEdit = (EditText)findViewById(R.id.ipInput);
-    	Log.v(TAG, mEdit.getText().toString());
-    }
-    
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+		et_input = (EditText) findViewById(R.id.ipInput);
+		Log.d("et_input", et_input.getText().toString());
+		Context context = getApplicationContext();
+		CharSequence text = "Connected!  See log";
+		int duration = Toast.LENGTH_SHORT;
 
-        public PlaceholderFragment() {
-        }
-        
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+        Thread cThread = new Thread(new ClientThread());
+        cThread.start();
     }
 
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	};
+	
+	public class ClientThread implements Runnable {
+		 
+        public void run() {  	
+            try {
+                InetAddress serverAddr = InetAddress.getByName(et_input.getText().toString());
+                Log.d("ClientActivity", et_input.getText().toString());
+                Log.d("ClientActivity", "C: Connecting...");
+                Socket socket = new Socket(serverAddr, SERVER_PORT);
+                connected = true;
+                while (connected) {
+                    try {
+                        Log.d("ClientActivity", "C: Sending command.");
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                                    .getOutputStream())), true);
+                        out.println("Hey Server!");
+                        Log.d("ClientActivity", "C: Sent.");
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        if (br.readLine() != null)
+                        	{
+                        	Log.d("ClientActivity", br.readLine());
+                        	}
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+        }
+    }
 }
