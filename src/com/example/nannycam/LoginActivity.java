e1f9853a0238ff2386b7be068d26cfdd47f470c4
Aaron Davis <magwe25@gmail.com>
2014-04-24 14:55:15 -0400

    Added authenticate activity and respective layout

---
+++ src/com/example/nannycam/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.nannycam;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Build;
+
+public class LoginActivity extends ActionBarActivity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+	}
+	
+    
+    public void login(View v) {
+    	
+    	
+    }
+
+}
