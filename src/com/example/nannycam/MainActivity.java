fbaa6a24996dcb148250476678f537f020390431
Aaron Davis <magwe25@gmail.com>
2014-04-24 23:19:10 -0400

    Fixed import bug from merge

--- src/com/example/nannycam/MainActivity.java
+++ src/com/example/nannycam/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.nannycam;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.Socket;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
